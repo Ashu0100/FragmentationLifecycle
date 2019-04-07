df47bf4821d5e85b35668450fa2d3b8cd68dd1bb
ashish <ashishpareek25feb@gmail.com>
2019-04-07 17:22:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fragmentationlifecycle/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.fragmentationlifecycle;
+
+import android.arch.lifecycle.Lifecycle;
+import android.content.Context;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG ="Lifecycle" ;
+    public MainActivity(){
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
+        return inflater.inflate(R.layout.activity_main,container,false);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+}
